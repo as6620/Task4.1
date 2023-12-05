1a76af2306f8f0c15fb18c0b710c58be504d36ef
as6620 <as6620@bs.amalnet.k12.il>
2023-12-05 09:34:01 +0200

    Initial commit

---
+++ app/src/main/java/com/example/task41/MainActivity.java
@@ -0,0 +1,15 @@
+package com.example.task41;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+    Button button;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
